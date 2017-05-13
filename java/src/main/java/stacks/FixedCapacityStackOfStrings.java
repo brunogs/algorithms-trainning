a144c9c144ea64415ff356c54ac805f73534bdf1
Bruno Silva <brunogs.sp@gmail.com>
2017-05-13 13:13:59 -0300

    implemented examples of stack: linked-list, fixed capacity and resizing array

---
+++ java/src/main/java/stacks/FixedCapacityStackOfStrings.java
@@ -0,0 +1,25 @@
+package stacks;
+
+public class FixedCapacityStackOfStrings {
+
+    private String[] s;
+    private int N = 0;
+
+    public FixedCapacityStackOfStrings(int capacity) {
+        s = new String[capacity];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public void push(String item) {
+        s[N++] = item;
+    }
+
+    public String pop() {
+        String item = this.s[--N];
+        s[N] = null;
+        return item;
+    }
+}
---
+++ java/src/main/java/stacks/ResizingArrayStackOfStrings.java
@@ -0,0 +1,39 @@
+package stacks;
+
+public class ResizingArrayStackOfStrings {
+
+    private String[] s;
+    private int N = 0;
+
+    public ResizingArrayStackOfStrings() {
+        s = new String[1];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public void push(String item) {
+        if (N == s.length) {
+            resize(2 * s.length);
+        }
+        s[N++] = item;
+    }
+
+    public String pop() {
+        String item = this.s[--N];
+        s[N] = null;
+        if (N > 0 && N == s.length/4) {
+            resize(s.length/2);
+        }
+        return item;
+    }
+
+    private void resize(int capacity) {
+        String[] copy = new String[capacity];
+        for (int i = 0; i < N; i++) {
+            copy[i] = s[1];
+        }
+        s = copy;
+    }
+}
