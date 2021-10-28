bf007f6da506171e0ee05e711611d67e86d62606
Bruno Silva <bruno.gomes@belezanaweb.com.br>
2021-10-27 23:37:15 -0300

    refactor: plusone.js

--- nodejs/plusone.js
+++ nodejs/plusone.js
@@ -1,18 +1,15 @@
 // const digits = [1,2,3]
-// const digits = [4,3,2,1]
+const digits = [4,3,2,1]
 // const digits = [0]
 // const digits = [9]
-const digits = [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]
+// const digits = [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]
 
 /**
  * @param {number[]} digits
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    let stringDigits = ""
-    for (let i = 0; i < digits.length; i++) {
-        stringDigits = stringDigits.concat(digits[i])
-    }
+    let stringDigits = digits.join('')
     const digitsPlusOne = BigInt(stringDigits) + 1n
     const numbersResult = []
     for (const i of digitsPlusOne.toString()) {
