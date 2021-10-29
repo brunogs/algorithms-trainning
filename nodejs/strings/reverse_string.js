ef22ea887a894566927b8197582bf64670676602
Bruno Silva <bruno.gomes@belezanaweb.com.br>
2021-10-29 05:01:07 -0300

    refactor: move files and add reverse_string.js

---
+++ nodejs/strings/reverse_string.js
@@ -0,0 +1,21 @@
+let s = ["h","e","l","l","o"]
+//let s = ["H","a","n","n","a","h"]
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let start = 0
+    let end = s.length - 1
+    while (start < end) {
+        const temp  = s[start]
+        s[start] = s[end]
+        s[end] = temp
+        start++
+        end--
+    }
+};
+
+reverseString(s)
+console.log(s)
